cdc5f2fe1886ae6f7e7a09c6c93132fa64d04d56
Huy <hchicken@gmail.com>
2022-09-08 13:23:20 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,32 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={{flex:2}}></View>
+      <View style={styles.box}><Text>Hello World</Text></View>
+       <Button 
+       onPress={()=> {alert('Xin chào')}}
+       title='Click me'
+        />
+      <View style={{flex:2}}></View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  box: {
+    flex : 1,
+    backgroundColor: 'skyblue',
+    width:500,
+    alignItems:'center',
+    justifyContent:'center'
+  },
+});
