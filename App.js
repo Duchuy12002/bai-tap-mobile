e25f04d64f02ae21c2f5f809073375397858a2c1
Duchuy12002 <112165422+Duchuy12002@users.noreply.github.com>
2022-09-08 18:15:11 +0700

    Update App.js

--- App.js
+++ App.js
@@ -1,32 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View style={{flex:2}}></View>
-      <View style={styles.box}><Text>Hello World</Text></View>
-       <Button 
-       onPress={()=> {alert('Xin chào')}}
-       title='Click me'
-        />
-      <View style={{flex:2}}></View>
+    <View style={{
+      alignItems: 'center',
+      flexDirection: 'row',
+      flex: 1,
+      display: 'flex',
+      justifyContent: 'center',
+    }}>
+      <View style={[styles.square, styles.red]}>
+        <Text style={{ fontWeight: '700' }}>Square 1</Text>
+      </View>
+      <View style={[styles.square, styles.aqua]}>
+        <Text style={{ fontWeight: '700' }}>Square 2</Text>
+      </View>
+      <View style={[styles.square, styles.green]}>
+        <Text style={{ fontWeight: '700' }}>Square 3</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    display: 'flex',
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  box: {
-    flex : 1,
-    backgroundColor: 'black',
-    width:500,
-    alignItems:'center',
-    justifyContent:'center'
+  square: {
+    margin: 20,
+    paddingVertical: 30,
+    paddingHorizontal: 40
   },
-});
+  red: {
+    backgroundColor: 'red',
+  },
+  aqua: {
+    backgroundColor: 'aqua',
+  },
+  green: {
+    backgroundColor: 'green',
+  }
+}
+);
